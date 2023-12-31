45ca89a979d9b8061a85394fb7ab58e7330ea9a1
Omar <omarabbasi16@gmail.com>
2023-12-31 16:55:01 -0500

    tested functions

--- Main.cpp
+++ Main.cpp
@@ -1,36 +1,51 @@
 #include <iostream>
 #include "./Vector.hpp"
 
+template <typename T>
+void LogVector(Vector<T>& v){
+    std::cout << "Size = " << v.Size() << ", Capacity = " << v.Capacity() << "Is Empty? =" << v.IsEmpty() << std::endl;
+    
+    for (int i = 0; i < v.Size(); i++){
+        std::cout << "items[" << i << "] = " << v[i] << std::endl;
+    }
+}
+
 int main(){
-   Vector<float> v;
-   v.PushBack(0);
-   v.PushBack(10);
-   v.PushBack(20);
-   v.PushBack(30);
-   v.PushBack(40);
-   v.PushBack(50);
-   v.PushBack(60);
-   v.PushBack(70);
-   v.PushBack(80);
-   v.PushBack(90);
-   v.PushBack(100);
-   v.PushBack(110);
-   v.PushBack(120);
-   v.PushBack(130);
-   v.PushBack(999);
-   v.PopBack();
-   v.Erase(5);
+    
+    //Initialize a vector 
+    Vector<float> v;
+  
+    //Initialize a vector with values
+    Vector<float> w(10, 1);
+    LogVector(w);
 
-   v.Insert(10, 500);
+    //Initialize a vector that is a copy of another vector
+    Vector<float> w2(w);
+    LogVector(w2); 
 
-   Vector<float> w;
-   w = v;
+    //Modify values within a vector
+    w[5] = 0;
+    LogVector(w);
+ 
+    //Set one vector equal to another
+    v = w;
+    LogVector(v);
+    
+    //Push a value to the end of a vector
+    v.PushBack(2);
+    LogVector(v);
 
-   for (int i = 0; i < w.Size(); i++){
-       std::cout << "items[" << i << "] = " << w[i] << std::endl;
-   }
+    //Pop the last value of a vector
+    v.PopBack();
+    LogVector(v);
 
-   std::cout << "Size = " << w.Size() << ", Capacity = " << w.Capacity() << std::endl;
+    //Insert a value into a vector
+    v.Insert(5, 11);
+    LogVector(v);
+
+    //Erase a value from a vector
+    v.Erase(5);   
+    LogVector(v);
 
     return 0;
 }
